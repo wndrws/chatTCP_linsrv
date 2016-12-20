314f0ab667bea7e591a726117b2fd7eaeb6cb05d
wndrws <wws.dev@gmail.com>
2016-12-21 00:03:22 +0300

    Номер посылки SRVMSG изменен с 0 на 10.

--- src/ClientRec.h
+++ src/ClientRec.h
@@ -8,7 +8,6 @@
 #include <pthread.h>
 #include "etcp.h"
 
-#define CODE_SRVMSG 0
 #define CODE_LOGINREQUEST 1
 #define CODE_LOGOUTREQUEST 2
 #define CODE_LOGINANSWER 3
@@ -18,6 +17,7 @@
 #define CODE_LOGOUTNOTIFY 7
 #define CODE_SRVERR 8
 #define CODE_HEARTBEAT 9
+#define CODE_SRVMSG 10
 
 #define CODE_INMSG 128
 #define CODE_OUTMSG 129
